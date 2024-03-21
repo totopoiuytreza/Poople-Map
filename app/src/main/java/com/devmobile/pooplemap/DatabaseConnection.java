e971277b78a0375d15da153742135c8bc79220af
Thomas Song <thomas.song@efrei.net>
2024-03-21 09:56:54 +0100

    initial config

---
+++ app/src/main/java/com/devmobile/pooplemap/DatabaseConnection.java
@@ -0,0 +1,71 @@
+package com.devmobile.pooplemap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DatabaseConnection {
+    private Connection connection;
+    private final String user;
+    private final String pass;
+    private final String url;
+    private boolean status;
+
+    public DatabaseConnection(String uri, String user, String password)
+    {
+        this.url = "jdbc:" + uri;
+        this.user = user;
+        this.pass = password;
+        System.out.println("Url:" + url);
+
+        connect();
+        //this.disconnect();
+        System.out.println("connection status:" + status);
+    }
+
+    private void connect()
+    {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Class.forName("org.postgresql.Driver");
+                    connection = DriverManager.getConnection(url, user, pass);
+                    status = true;
+                    System.out.println("connected:" + status);
+                }
+                catch (Exception e)
+                {
+                    status = false;
+                    System.out.print(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try
+        {
+            thread.join();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            this.status = false;
+        }
+    }
+
+    public Connection getExtraConnection()
+    {
+        Connection c = null;
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(url, user, pass);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return c;
+    }
+}
--- app/src/main/java/com/devmobile/pooplemap/MainActivity.java
+++ app/src/main/java/com/devmobile/pooplemap/MainActivity.java
@@ -3,12 +3,22 @@ package com.devmobile.pooplemap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String DbUri = getResources().getString(R.string.POSTGRESQL_ADDON_URI);
+        String DbUser = getResources().getString(R.string.POSTGRESQL_ADDON_USER);
+        String DbPass = getResources().getString(R.string.POSTGRESQL_ADDON_PASSWORD);
+
+        DatabaseConnection db = new DatabaseConnection(DbUri, DbUser, DbPass);
     }
 }
