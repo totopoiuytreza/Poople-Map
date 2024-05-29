ac4713c8afa1ed8d029e8af1998620e8563527d4
Thomas Song <thomas.song@efrei.net>
2024-05-29 23:29:07 +0200

    minimum for map functionnality without backend

--- app/src/main/java/com/devmobile/pooplemap/fragments/HomeFragment.java
+++ app/src/main/java/com/devmobile/pooplemap/fragments/HomeFragment.java
@@ -1,22 +1,55 @@
 package com.devmobile.pooplemap.fragments;
 
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.Manifest;
 import com.devmobile.pooplemap.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
 
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    private SearchView mapSearch;
+    private Marker searchBarMarker;
+    private Marker currentLocationMarker;
+
+    private ImageButton currentLocationButton;
+    private ImageButton addLocationButton;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 123;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,12 +61,146 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
 
+        // Initialize the FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
+
+        // Request location permission if not already granted
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate the view for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mapSearch = view.findViewById(R.id.search_map);
+
+        SupportMapFragment map = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.user_map);
+        map.getMapAsync(this);
+        mapSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = mapSearch.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null){
+                    Geocoder geocoder = new Geocoder(getContext());
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                // Remove the previous marker
+                if (searchBarMarker != null) {
+                    searchBarMarker.remove();
+                }
+                searchBarMarker= mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        map.getMapAsync(this);
+
+
+        currentLocationButton = view.findViewById(R.id.current_location);
+        addLocationButton = view.findViewById(R.id.add_location);
+
+        addLocationButton.setOnClickListener(v -> {
+            showBottomDialog();
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Check if the user has granted location permission
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Get the user's current location
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(requireActivity(), location -> {
+                        if (location != null) {
+                            LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            currentLocationMarker = mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                        }
+                    });
+            currentLocationButton.setOnClickListener(v -> {
+                // Get the user's current location
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(requireActivity(), location -> {
+                            if (location != null) {
+                                LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                // Remove the previous marker
+                                if (currentLocationMarker != null) {
+                                    currentLocationMarker.remove();
+                                }
+                                // Add a new marker for the user's current location
+                                currentLocationMarker = mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                                // Move the camera to the user's current location
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                            }
+                        });
+            });
+
+        } else {
+            // Request location permission if not already granted
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission granted, you can now get the user's location
+                onMapReady(mMap);
+            } else {
+                // Location permission denied
+                // Show a message to the user that location permission is required to use the app
+
+                Toast.makeText(requireContext(), "Location permission is required to use the app", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void showBottomDialog() {
+        // Show the bottom dialog
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheet_layout);
+
+
+        ImageView clear = dialog.findViewById(R.id.clear_button);
+
+        clear.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
